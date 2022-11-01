11ac1a3746f634901050a0fef803e75369dbae82
sidy8202 <diakitesidy3@gmail.com>
2022-11-01 21:35:42 +0100

    My first commit

---
+++ assets/js/details.js
@@ -0,0 +1,47 @@
+  // async function loadUsers() {
+  //   const response = await fetch("https://fakestoreapi.com/users?limit=5")
+  //       return response.json(); 
+  // }
+  
+  // document.addEventListener("DOMContentLoaded", async () => {
+  //   try {
+  //     const users = await loadUsers();
+  //     const divContainer = document.getElementById('content');
+  //     users.forEach(user => {
+  //         const paragraphElem = document.createElement('p');
+  //         paragraphElem.innerText = `ID: ${user.id} \n Email: ${user.Email} \n Username: ${user.username} \n Password: ${user.password}`;
+  //         divContainer.appendChild(paragraphElem);
+  //     });
+  //   } catch (e) {
+  //     console.log('ERROR');
+  //     console.log(e);
+  //   }
+  // });
+
+  
+
+// ENd of new table
+
+
+// {/* <td><a href ="details.html${element.id} " class="details btn btn-success">detailp</a></td> */}
+
+const table = document.getElementById('tableau')
+
+async function getResponse() {
+	const response = await fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>console.log(json)) }
+       
+fetch('https://fakestoreapi.com/products')
+  .then(
+  // recuperer les valeurs que contient response
+  response => response.json())
+  .then((json) => json.forEach([cle,tableElements]))
+
+  function tableElements (element, index, arr){
+    alert(cle);
+  }  
+
+
+ var getId = document.location.href; 
+
